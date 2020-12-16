0cf8153509c6848aa1449d79eead0c039655f877
Mariann <tapferm@gmail.com>
2020-12-16 16:42:39 +0200

    https and request päringud

--- helpers/Upload_to_Strapi.js
+++ helpers/Upload_to_Strapi.js
@@ -1,51 +1,78 @@
-let request = require('request');
+
 var fs = require('fs');
-const { strapiAuth } = require('./strapiAuth.js')
-
-// // let file = new URL('https://saal.entu.ee/api2/file-21645')
-// function GetPic(id){
-//     var request = require('request');
-//     var options = {
-//       'method': 'GET',
-//       'url': 'https://saal.entu.ee/api2/file-'+id,
-//       'headers': {
-//       }
-//     };
-//     request(options, function (error, response) {
-//       if (error) throw new Error(error);
-//       console.log(response.body);
-//     });
-// }
-
-// GetPic()
+var request = require('request');
+const {strapiAuth} = require('./strapiAuth.js')
+const https = require('follow-redirects').https;
+var FormData = require('form-data');
 
 var TOKEN = ''
 
-async  function picsToStrapi(){
+
+async function picsToStrapi() {
     if (TOKEN === '') {
-        TOKEN = await strapiAuth() // TODO: setting global variable is no a good idea
-         console.log('Bearer', TOKEN)
+        TOKEN = await strapiAuth()
     }
+    // `Bearer ${TOKEN}`
+
+    // let options = {
+    //   'method': 'POST',
+    //   'hostname': 'a.saal.ee',
+    //   'path': '/upload',
+    //   'headers': {
+    //     'Authorization': `Bearer ${TOKEN}`
+    //   },
+    //   'maxRedirects': 20
+    // };
+
+    // const req = https.request(options, (res) => {
+    //   let chunks = [];
+
+    //   res.on("data", (chunk) => {
+    //     chunks.push(chunk);
+    //   });
+
+    //   res.on("end", (chunk) => {
+    //     let body = Buffer.concat(chunks);
+    //     console.log(body.toString());
+    //   });
+
+    //   res.on("error", (error) => {
+    //     console.error(error);
+    //   });
+    // });
+
+    // var postData = new FormData();
+    // postData.append('files', fs.createReadStream('assets/images/testimage_landscape_full.jpg'));
+
+    // let postData = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"files\"; filename=\"social classroom app.png\"\r\nContent-Type: \"{Insert_File_Content_Type}\"\r\n\r\n" + fs.readFileSync('/Users/Mariann/Desktop/social classroom app.png') + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--";
+
+    // req.setHeader('content-type', 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW');
+
+    // req.write(postData);
+
+    // req.end();
+
+
 
     let options = {
-      'method': 'POST',
-      'url': 'https://a.saal.ee/upload',
-      'headers': {
-        'Authorization': `Bearer ${TOKEN}`
-      },
-      formData: {
-        'files': {
-          'value': fs.createReadStream('assets/images/testimage_landscape_full.jpg'),
-          'options': {
-            'filename': 'testimage_landscape_full.jpg',
-            'contentType': null
-          }
+        'method': 'POST',
+        'url': 'https://a.saal.ee/upload',
+        'headers': {
+            'Authorization': `Bearer ${TOKEN}`
+        },
+        formData: {
+            'files': {
+                'value': fs.createReadStream('assets/images/testimage_hytt.jpg'),
+                'options': {
+                    'filename': 'testimage_hytt.jpg',
+                    'contentType': null
+                }
+            }
         }
-      }
     };
     request(options, function (error, response) {
-      if (error) throw new Error(error);
-      console.log(response.body);
+        if (error) throw new Error(error);
+        console.log(response.body);
     });
 
 }
