4b78ebf7cc873cdd57b5da5e8fe9147ca165dcf7
Martin Pennaste <pennaste04@gmail.com>
2021-01-04 16:52:00 +0200

    Festival press, performance coverages, magazine/resident audios/videos

--- helpers/performances_from_yaml.js
+++ helpers/performances_from_yaml.js
@@ -21,11 +21,11 @@ for (const lang of LANGUAGES) {
 
     for (const performance of STRAPIDATA_PERFORMANCES) {
 
-        // if (['6865', '6858', '6538'].includes(performance.remote_id)){
+        if (['6865', '6858', '6538', '5429', '5810'].includes(performance.remote_id)){
 
-        // } else {
-        //     continue
-        // }
+        } else {
+            continue
+        }
 
         // let performance.category = STRAPIDATA_CATEGORIES.filter( category => {
         //     performance.remote_id === category.
@@ -63,6 +63,12 @@ for (const lang of LANGUAGES) {
 
             if (performance.X_pictures) {
                 performance.X_pictures = sort_pictures(performance.X_pictures)
+            }
+            if (performance.coverages) {
+                performance.coverages = performance.coverages.sort((a, b) => new Date(b.publish_date)-new Date(a.publish_date))
+                if(performance.coverages.filter(p => p.media).length > 0) {
+                    console.log('bla: ', performance.remote_id);
+                }
             }
             performance.data = { categories: `/_fetchdir/categories.${lang}.yaml`}
 
