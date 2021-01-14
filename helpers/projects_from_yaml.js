7347a92b5d5e96afa1e3e3b4f901d4aee945d563
Martin Pennaste <pennaste04@gmail.com>
2021-01-14 17:07:59 +0200

    Events target projects and other

--- helpers/projects_from_yaml.js
+++ helpers/projects_from_yaml.js
@@ -3,7 +3,7 @@ const yaml = require('js-yaml')
 const path = require('path')
 const addConfigPathAliases = require('./add_config_path_aliases.js')
 
-const targeted = process.argv[2] === '-t' && process.argv[3] ? true : false
+const targeted = process.argv[2] === '-t' ? true : false
 const rootDir =  path.join(__dirname, '..')
 const sourceDir = path.join(rootDir, 'source')
 const fetchDir = path.join(sourceDir, '_fetchdir')
@@ -16,29 +16,23 @@ const strapi_projects = STRAPIDATA_EVENTS
     .filter((event) => event.type === 'project')
     .sort((a, b) => (a.order || 0) - (b.order || 0))
 
-const isProject = strapi_projects.map(p => p.id.toString()).includes(process.argv[3])
+for (const lang of LANGUAGES) {
+    const projects = []
 
-if ((isProject && targeted) || !targeted) {
-
-    for (const lang of LANGUAGES) {
-        const projects = []
-
-        for (const strapi_project of strapi_projects) {
-            const project = {
-                id: strapi_project.id,
-                order: strapi_project.order || null,
-                name: strapi_project['name_' + lang],
-                description: strapi_project['description_' + lang],
-            }
-            projects.push(project)
+    for (const strapi_project of strapi_projects) {
+        const project = {
+            id: strapi_project.id,
+            order: strapi_project.order || null,
+            name: strapi_project['name_' + lang],
+            description: strapi_project['description_' + lang],
         }
-        const projectsYAMLPath = path.join(fetchDir, `projects.${lang}.yaml`)
-        const projectsYAML = yaml.safeDump(projects, { 'noRefs': true, 'indent': '4' })
-        fs.writeFileSync(projectsYAMLPath, projectsYAML, 'utf8')
+        projects.push(project)
     }
+    const projectsYAMLPath = path.join(fetchDir, `projects.${lang}.yaml`)
+    const projectsYAML = yaml.safeDump(projects, { 'noRefs': true, 'indent': '4' })
+    fs.writeFileSync(projectsYAMLPath, projectsYAML, 'utf8')
+}
 
-    if (targeted) {
-        addConfigPathAliases(['projects/'])
-    }
-
+if (targeted) {
+    addConfigPathAliases(['projects/'])
 }
