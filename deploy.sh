48f7a27f2c8cd95aba3884acb7a985261912f4a5
Liis <liis.kasper@gmail.com>
2021-01-27 17:42:33 +0200

    test deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+SECONDS=0
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+echo $PWD
+
+echo '\nMaking copy from live site\n'
+cp -a /srv/www/. /srv/www-bak/`date +"%Y-%m-%d_%H-%M-%S"`/
+
+
+echo '\nReplace live site'
+rsync -avh /srv/ssg/build/. /srv/www/  --delete-after
+
