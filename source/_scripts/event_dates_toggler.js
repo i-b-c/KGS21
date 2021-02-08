c34af2f9d59b37da059f630887b2f6b6fdad5b6b
Martin Pennaste <pennaste04@gmail.com>
2021-02-08 11:34:03 +0200

    Hotfix: Performance events toggler

--- source/_scripts/event_dates_toggler.js
+++ source/_scripts/event_dates_toggler.js
@@ -6,14 +6,13 @@ function performanceEventToggler(timing) {
     for( var event in eventElement){
         if (eventElement.hasOwnProperty(event)) {
 
-            var eventEnd = eventElement.getAttribute('end-time') || null
+            var eventEnd = eventElement[event].getAttribute('end-time') || null
             var evendEndDate = eventEnd ? new Date(eventEnd) : null
-            var eventStatus = eventElement.getAttribute('sales-status') || null
+            var eventStatus = eventElement[event].getAttribute('sales-status') || null
             var eventStart = eventElement[event].getAttribute('start-time')
             var eventStartTime = eventStart ? new Date(eventStart) : null
 
             if (timing === 'future_events'){
-                console.log(eventEnd, evendEndDate,eventStatus);
                 if (eventStartTime && currentTime <= eventStartTime) {
                     futureEventCount++
                 } else if (evendEndDate && evendEndDate >= currentTime && eventStatus === 'online') {
