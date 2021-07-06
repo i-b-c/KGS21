3431d5aa3425fc8b2cbd147e067c52120c058010
Martin Pennaste <pennaste04@gmail.com>
2021-07-06 11:50:31 +0300

    Debug article build

--- ssg/build_article.sh
+++ ssg/build_article.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 echo $PWD
 
-printf "\nBuilding...\n"
+printf "\nBuilding (target article)...\n"
 
 printf '\n\n---------- Creating separate YAML files from data ----------\n\n'
 
