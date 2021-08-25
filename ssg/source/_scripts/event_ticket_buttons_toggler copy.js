cf5b5cffa0e9e4a1b0b51a758d35e87e03cab956
Martin Pennaste <pennaste04@gmail.com>
2021-08-25 22:15:49 +0300

    Event ticket button visibility until event end

--- ssg/source/_scripts/event_ticket_buttons_toggler copy.js
+++ ssg/source/_scripts/event_ticket_buttons_toggler copy.js
@@ -2,15 +2,18 @@ function toggleEventButtons() {
     var eventElement = document.querySelectorAll('[type-name="ticket-button"]')
     var currentTime = new Date()
 
-    for(var event in eventElement){
-
-        if (eventElement.hasOwnProperty(event) && eventElement[event].getAttribute('event-sales-status') === 'online' && eventElement[event].getAttribute('event-end')
-            && currentTime <= new Date(eventElement[event].getAttribute('event-end'))) {
-            eventElement[event].style.display = ''
-        } else
-        if (eventElement.hasOwnProperty(event) && currentTime <= new Date(eventElement[event].getAttribute('event-start'))){
-            eventElement[event].style.display = ''
+    for (var event in eventElement) {
+        if (eventElement.hasOwnProperty(event)) {
+            var eventEndTime = new Date(eventElement[event].getAttribute('event-end'))
+            var eventStartTime = new Date(eventElement[event].getAttribute('event-start'))
+            if (eventElement[event].getAttribute('event-sales-status') === 'online' && eventEndTime
+                && currentTime <= eventEndTime) {
+                eventElement[event].style.display = ''
+            } else if (eventEndTime && currentTime <= eventEndTime) {
+                eventElement[event].style.display = ''
+            } else if (eventStartTime && currentTime <= eventStartTime) {
+                eventElement[event].style.display = ''
+            }
         }
-
     }
 }
